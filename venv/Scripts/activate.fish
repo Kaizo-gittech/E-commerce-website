5195cea7ca9c85a38a9970dd60b4aea4ba25d237
Kaizo-gittech <kabirthapakepitmello@gmail.com>
2026-08-13 21:50:57 +0530

    new requirements commit

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\Kabir Thapa\Desktop\Anistock\venv'
+set -gx VIRTUAL_ENV 'C:\Users\Kabir Thapa\OneDrive\Desktop\Anistock\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
